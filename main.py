3888073c5497caf662906264c2849670f0c17f96
Wolfgang Bumiller <wry.git@bumiller.com>
2014-01-04 11:00:23 +0100

    installer baseclass

--- main.py
+++ main.py
@@ -1,96 +1,97 @@
 #!/usr/bin/python3
 
-import part
 import utils
 import i_parted
-from geom import geom
 
 import os
-import curses
 import atexit
-from curses.textpad import Textbox, rectangle
-
-###############################################################################
-
-Main = lambda: None
-
-###############################################################################
-
-def print_tables(tables):
-    # now print the layout
-    for t in tables:
-        print('%s (%s) [%u, %u]' % (t.name, t.scheme, t.first, t.last))
-        for p in t.partitions:
-            print('\t %s (%s) [%u, %u]' % (p.name, p.partype, p.start, p.end))
+import curses
+import json
 
 def main():
-    Main.screen = curses.initscr()
-    atexit.register(gui_shutdown)
-    try:
-        setup_gui()
-        start()
-        destroy_gui()
-    except KeyboardInterrupt:
-        pass
-    except Exception as inst:
+    installer = Installer()
+    installer.main()
+
+###############################################################################
+
+class Installer(object):
+    def __init__(self):
+        self.home        = os.environ['HOME']
+        self.config_file = self.home + '/absd-installer.json'
+
+        self.fstab = []
+
         try:
-            atexit.unregister(gui_shutdown)
-            destroy_gui()
-        except:
+            with open(self.config_file, 'r', encoding='utf-8') as f:
+                data = json.load(f)
+                self.fstab = data['fstab']
+        except (FileNotFoundError, PermissionError) as inst:
+            print(inst)
+
+    def save(self):
+        data = {
+            'fstab': self.fstab
+        }
+        with open(self.config_file, 'w', encoding='utf-8') as f:
+            json.dump(self.data, f, sort_keys=True,
+                      indent=4, separators=(',', ':'))
+
+    def main(self):
+        def exit_hook():
+            try:
+                self.end_gui()
+            except:
+                pass
+
+        atexit.register(exit_hook)
+        self.screen = curses.initscr()
+
+        try:
+            self.setup_gui()
+            self.run()
+            atexit.unregister(exit_hook)
+            exit_hook()
+        except KeyboardInterrupt:
             pass
-        raise inst
+        except Exception as inst:
+            atexit.unregister(exit_hook)
+            exit_hook()
+            # Now that the terminal can actually display text again
+            # rethrow the exception
+            raise inst
 
-def setup_gui():
-    curses.start_color()
-    curses.savetty()
-    curses.noecho()
-    curses.cbreak()
-    curses.curs_set(0)
+    def setup_gui(self):
+        curses.start_color()
+        curses.savetty()
+        curses.noecho()
+        curses.cbreak()
+        curses.curs_set(0)
+        self.screen.keypad(True)
+        self.resize_event()
 
-    Main.screen.keypad(True)
+    def end_gui(self):
+        curses.resetty()
+        curses.endwin()
+        os.system('stty sane')
 
-    # fstab entries...
-    Main.fstab       = []
+    def resize_event(self):
+        self.size = self.screen.getmaxyx()
+        self.screen.refresh()
 
-    # callbacks
-    Main.resize_event  = resize_event
-    Main.translate_key = translate_key
-    Main.get_key       = get_key
+    def get_key(self):
+        key  = self.screen.getch()
+        name = utils.translate_key(key)
+        if key == 0x7f:
+            key = curses.KEY_BACKSPACE
+        elif key == 0x0a:
+            key = curses.KEY_ENTER
+        return (key, name)
 
-    resize_event()
+    def run(self):
+        with i_parted.Parted(self) as parted:
+            parted.run()
 
-def destroy_gui():
-    gui_shutdown()
-    atexit.unregister(gui_shutdown)
-
-def gui_shutdown():
-    curses.resetty()
-    curses.endwin()
-    os.system('stty sane')
-
-def resize_event():
-    Main.size = Main.screen.getmaxyx()
-    Main.screen.refresh()
-
-def translate_key(key):
-    try:
-        return curses.keyname(key)
-    except ValueError:
-        return key
-
-def get_key():
-    key  = Main.screen.getch()
-    name = translate_key(key)
-    if key == 0x7f:
-        key = curses.KEY_BACKSPACE
-    elif key == 0x0a:
-        key = curses.KEY_ENTER
-    return (key, name)
-
-def start():
-    Main.screen.refresh()
-    with i_parted.Parted(Main) as parted:
-        parted.run()
+###############################################################################
 
 if __name__ == '__main__':
     main()
